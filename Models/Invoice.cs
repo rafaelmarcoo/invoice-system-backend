c8a8787ce4f49ed6ec644691b29af1098b09fc46
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-27 13:36:08 +1300

    Create Invoice.cs

---
+++ Models/Invoice.cs
@@ -0,0 +1,14 @@
+﻿namespace invoice_system_backend.Models
+{
+    public class Invoice
+    {
+        public string InvoiceCode { get; set; }
+        public string Name { get; set; }
+        public DateOnly DateSent { get; set; }
+        public DateOnly DateDue { get; set; }
+        public int Amount { get; set; }
+        public int GST { get; set; }
+        public bool IsPaid { get; set; }
+        public bool IsRecurring { get; set; }
+    }
+}
