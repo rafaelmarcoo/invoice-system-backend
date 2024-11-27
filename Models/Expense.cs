d7946356b0adfe26d5048120d147bbf7448febdd
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-27 13:39:15 +1300

    Create Expense.cs

---
+++ Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace invoice_system_backend.Models
+{
+    public class Expense
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IFormFile File { get; set; }
+        public DateOnly Date { get; set; }
+        public int Amount { get; set; }
+    }
+}
