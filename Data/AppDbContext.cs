52c5725174ce27628a44bb4bb4581190c455a273
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-27 13:43:21 +1300

    DbSet the models in AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using invoice_system_backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace invoice_system_backend.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+
     }
 }
