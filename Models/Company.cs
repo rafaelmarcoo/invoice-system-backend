e397a9f7d2ab678ac9c307f29d72bee62a5bc32f
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-27 13:29:23 +1300

    Create Company.cs

---
+++ Models/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace invoice_system_backend.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
