c6f5860ff9894c1ce996ef2ac5225aa346c29664
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-27 13:31:48 +1300

    Create Client.cs

---
+++ Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System.IO.Compression;
+
+namespace invoice_system_backend.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Zip { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string InvoiceCode { get; set; }
+    }
+}
