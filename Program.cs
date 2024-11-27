e7d9ebbc3327c7721fca7faac9dbdb0ad89f3766
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-27 13:26:14 +1300

    Connecting PostgreSQL with ASP.NET

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
+using invoice_system_backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Retrieve password from environment variable
+var password = Environment.GetEnvironmentVariable("PG_PASSWORD");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    .Replace("ENV_DB_PASSWORD", password);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString(connectionString)));
+builder.Services.AddEndpointsApiExplorer();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
