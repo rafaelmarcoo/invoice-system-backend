b3b9458d2337a646f3b8ac00b3aaf1a546c47d17
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-11-29 17:38:38 +1300

    Create ClientController.cs

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using invoice_system_backend.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace invoice_system_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ClientController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClientList()
+        {
+            var clientList = await _context.Clients.ToListAsync();
+            return Ok(clientList);
+        }
+    }
+}
