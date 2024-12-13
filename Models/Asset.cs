6bf0b3064404c90168a0b7b3e659394e0621e939
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-13 16:37:20 +1300

    Create Asset.cs

---
+++ Models/Asset.cs
@@ -0,0 +1,19 @@
+﻿namespace invoice_system_backend.Models
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string DatePurchased { get; set; }
+
+        public string DepreciationType { get; set; }
+
+        public decimal OriginalValue { get; set; }
+
+        public int UsefulLife { get; set; }
+    }
+}
