dc77b5b2ff8d8fa75f52cca869e6e7ccba7b256c
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-20 16:49:31 +1300

    Cleaning up - Assets

--- Models/Asset.cs
+++ Models/Asset.cs
@@ -1,21 +1,31 @@
-﻿namespace invoice_system_backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace invoice_system_backend.Models
 {
     public class Asset
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public string DatePurchased { get; set; }
 
+        [Required]
         public string DepreciationType { get; set; }
 
+        [Required]
         public decimal DepreciationRate { get; set; }
 
+        [Required]
         public decimal OriginalValue { get; set; }
 
+        [Required]
         public int UsefulLife { get; set; }
 
     }
