6c98c2161a6c96ddbf1438692f2bf5bbe92e080e
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-21 17:20:25 +1300

    Email Service - Created EmailController

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,25 @@
+﻿using invoice_system_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace invoice_system_backend.Controllers
+{  
+    [ApiController]
+    [Route("apli/[controller")]
+    public class EmailController : Controller
+    {
+        private readonly EmailService _emailService;
+
+        public EmailController(EmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost("send-email")]
+        public async Task<IActionResult> SendEmail([FromBody] Email email)
+        {
+            await _emailService.SendEmailAsync(email.To, email.Subject, email.Body);
+
+            return Ok("Email sent successfully");
+        }
+    }
+}
