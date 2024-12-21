a92eb12c167feefe58ef3845a3f0659224a2d863
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-12-21 17:13:05 +1300

    Email Service - Created EmailService

---
+++ Controllers/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Microsoft.Extensions.Configuration;
+
+namespace invoice_system_backend.Controllers
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            var emailSettings = _config.GetSection("EmailSettings");
+
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress("Your App Name", emailSettings["SenderEmail"]));
+            email.To.Add(new MailboxAddress("", to));
+            email.Subject = subject;
+
+            email.Body = new TextPart("html")
+            {
+                Text = body
+            };
+
+            using(var smtp = new SmtpClient())
+            {
+                await smtp.ConnectAsync(emailSettings["SMTPServer"], int.Parse(emailSettings["Port"]), MailKit.Security.SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync(emailSettings["SenderEmail"], emailSettings["SenderPassword"]);
+                await smtp.SendAsync(email);
+                await smtp.DisconnectAsync(true);
+            }
+        }
+    }
+}
