51e2105a7a1cfca4bfb16d08474e3582fdba2053
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2025-01-23 12:27:45 +1300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
